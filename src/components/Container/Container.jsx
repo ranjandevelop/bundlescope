73730f527f7464d1b3cbc0bea50b08644a0588cd
ranjandevelop <ranjanprasad745@gmail.com>
2025-12-08 09:58:58 +0530

    Fix: Screen height

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,6 +1,6 @@
 const Container = ({ children }) => {
   return (
-    <div className="relative min-h-full dark">
+    <div className="relative">
       {/* <img
         className="absolute left-0 top-0 -z-20 h-screen w-full object-cover"
         // src="https://tailkits.com/ui/iframe/assets/img/bg-5.png"
--- src/pages/Package/Package.jsx
+++ src/pages/Package/Package.jsx
@@ -74,7 +74,7 @@ const Package = () => {
       </header>
 
       {/* Content */}
-      <div className="w-full h-screen bg-neutral-900 lg:px-65 ">
+      <div className="w-full lg:h-screen bg-neutral-900 lg:px-65">
         <div className="p-4 sm:p-6 space-y-4 sm:space-y-6">
           <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
             {/* Downlaods */}
